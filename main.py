697ddacce2fadbaa3199c4b2fe84d32050a98f57
Aalekh Patel <aalekh.gwpeck.7998@gmail.com>
2021-07-23 11:11:01 -0500

    Add markovify and generate tweets.

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-#import markovify
+import markovify
 
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import cross_origin
 import json
 import random
+import datetime
+
 
 app = Flask(__name__, static_url_path='/static')
+
+
+def random_date(
+        start=datetime.datetime.strptime('1/1/2008 1:30 PM', '%m/%d/%Y %I:%M %p'),
+        end=datetime.datetime.strptime('8/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
+    ):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + datetime.timedelta(seconds=random_second)
+
 
 def stream():
     with open("trump_tweets.json", "r") as f:
         data = json.loads(f.read())
+
+    model = markovify.Text("\n".join(map(lambda x: x['text'], data)))
+    print(model)
+
     while True:
-        yield random.choice(data)
+        if random.random() <= .6:
+            yield {
+                    'text': '[GENERATED] ' + model.make_short_sentence(400),
+                    'favorites': random.randrange(100, 40000),
+                    'retweets': random.randrange(100, 40000),
+                    'date': str(random_date())
+                }
+        else:
+            yield random.choice(data)
 
 
 tweet_stream = stream()
