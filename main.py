582f3a384444564a6211a1a0090db1159d87dd99
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2021-07-24 22:09:40 -0500

    Integrate MongoDB for querying Tweets.

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-import markovify
-
+import os
+from random import sample
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import cross_origin
-import json
-import random
-import datetime
-import html
+import pymongo
+
+
+def get_db_client():
+    
+    USERNAME=os.getenv("USERNAME")
+    PASSWORD=os.getenv("PASSWORD")
+    DATABASE=os.getenv("DATABASE")
+    CLUSTER=os.getenv("CLUSTER")
+
+    HOST = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER}/{DATABASE}?retryWrites=true&w=majority"
+    return pymongo.MongoClient(HOST)
 
 
 app = Flask(__name__, static_url_path='/static')
 
 
-
-
 @app.route("/api/v1/tweet", methods=['GET'])
 @cross_origin()
 def tweet():
-    return jsonify(next(tweet_stream))
+    tweets = (
+        get_db_client()
+        .get_database(os.getenv("DATABASE"))
+        .get_collection("Tweet")
+    )
+
+    random_tweet = tweets.aggregate([{ 'sample': {'size': 1 }}])
+
+    return jsonify(random_tweet)
 
 @app.route("/", methods=["GET"])
 def root():
